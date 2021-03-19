aba963cd7d587ec3b2bde04a2d2ff7c25bf5abf4
CoderIllusionist <75251048+CoderIllusionist@users.noreply.github.com>
2021-03-19 12:59:45 +0100

    Added styling

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -20,53 +20,107 @@ function OrderForm({test}) {
         console.log(data)
     }
 
+    const watchCheck = watch("check", false); // supply default value as second argument
+    const radioButtons = watch("delivery", false);
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="firstname">Voornaam</label>
-                <input name="firstname" ref={register({required: true})} type="text"/>
+            <div className="form-container">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-names two-columns column">
+                        <div className="item">
+                            <label htmlFor="firstname">Voornaam</label>
+                            <input name="firstname" ref={register({required: true})} type="text"/>
+                        </div>
+                        <div className="item">
+                            <label htmlFor="lastname">Achternaam</label>
+                            <input name="lastname" ref={register({required: true})} type="text"/>
+                        </div>
+                    </div>
 
-                <label htmlFor="lastname">Achternaam</label>
-                <input name="lastname" ref={register({required: true})} type="text"/>
+                    <div className="form-age one-column column">
+                        <div className="item">
+                            <label htmlFor="age">Leeftijd</label>
+                            <input type="number" name="age"
+                                   ref={register({
+                                       required: true, valueAsNumber: true, min: 18, pattern: {
+                                           value: /^-?[0-9]\d*\.?\d*$/i,
+                                           message: ""
+                                       }
+                                   })} type="text"/>
+                        </div>
+                        {errors.age && errors.age.type == "required" && (<p>This is required</p>)}
+                        {errors.age && errors.age.type == "pattern" && (<p>This should be only numbers</p>)}
+                        {errors.age && errors.age.type == "min" && (<p>Minimal age is 18</p>)}
+                    </div>
+                    <div className="form-address two-columns column">
+                        <div className="item">
+                            <label htmlFor="zipcode">Postcode</label>
+                            <input type="text" name="zipcode"
+                                   ref={register({
+                                       required: true, pattern: {
+                                           value: /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i,
+                                           message: ""
+                                       }
+                                   })} type="text"/>
+                            {errors.zipcode && errors.zipcode.type == "pattern" && (
+                                <p>This is not a valid Dutch zip code!</p>)}
+                        </div>
+                        <div className="item">
+                            <label htmlFor="number">Huisnummer</label>
+                            <input type="number" name="home"
+                                   ref={register({
+                                       required: true, valueAsNumber: true, min: 18, pattern: {
+                                           value: /^-?[0-9]\d*\.?\d*$/i,
+                                           message: ""
+                                       }
+                                   })} type="text"/>
+                            {errors.home && errors.home.type == "pattern" && (<p>This should be only numbers</p>)}
+                        </div>
+                    </div>
+                    <div className="form-delivery-options five-columns column">
+                        <div className="radio-button">
+                            <label htmlFor="every-week">Elke week</label>
+                            <input name="delivery" ref={register({required: true})} type="radio" id="every-week"
+                                   value="every-week"/>
+                        </div>
+                        <div className="radio-button">
 
-                {errors.age && errors.age.type == "required" && (<p>This is required</p>)}
-                {errors.age && errors.age.type == "pattern" && (<p>This should be only numbers</p>)}
-                {errors.age && errors.age.type == "min" && (<p>Minimal age is 18</p>)}
-                <label htmlFor="age">Leeftijd</label>
-                <input type="number" name="age"
-                       ref={register({required: true, valueAsNumber: true, min: 18, pattern: {
-                               value: /^-?[0-9]\d*\.?\d*$/i,
-                               message: ""
-                           }})} type="text"/>
+                            <label htmlFor="weekly">Om de week</label>
+                            <input name="delivery" ref={register({required: true})} id="weekly" type="radio"
+                                   value="weekly"/>
+                        </div>
+                        <div className="radio-button">
 
-                {errors.home && errors.home.type == "pattern" && (<p>This should be only numbers</p>)}
-                <label htmlFor="number">Huisnummer</label>
-                <input type="number" name="home"
-                       ref={register({required: true, valueAsNumber: true, min: 18, pattern: {
-                               value: /^-?[0-9]\d*\.?\d*$/i,
-                               message: ""
-                           }})} type="text"/>
+                            <label htmlFor="every-month">Iedere maand</label>
+                            <input name="delivery" ref={register({required: true})} id="every-month" type="radio"
+                                   value="every-month"/>
+                        </div>
+                        <div className="radio-button">
+                            <label htmlFor="else">Anders</label>
+                            <input name="delivery" ref={register({required: true})} id="else" type="radio"
+                                   value="else"/>
+                        </div>
+                        {radioButtons === "else" && (
+                            <div className="one-column column">
+                                <div className="item">
+                                <label htmlFor="else-textbox">Anders, namelijk:</label>
+                                <input name="else-textbox" ref={register({required: true})} type="text"/>
+                                </div>
+                            </div>
 
-                <label htmlFor="every-week">Elke week</label>
-                <input name="delivery" ref={register({required: true})} type="radio" id="every-week"
-                       value="every-week"/>
+                        )}
+                    </div>
+                    <div className="form-comment one-column column">
+                        <div className="item">
+                            <label htmlFor="comment">Opmerking
+                                <textarea name="textarea" ref={register({required: true})}
+                                          id="comment"></textarea></label>
+                        </div>
+                    </div>
+                    <button type="submit" ref={register}>Verzend</button>
 
-                <label htmlFor="weekly">Om de week</label>
-                <input name="delivery" ref={register({required: true})} id="weekly" type="radio" value="weekly"/>
-
-                <label htmlFor="every-month">Iedere maand</label>
-                <input name="delivery" ref={register({required: true})} id="every-month" type="radio"
-                       value="every-month"/>
-
-                <label htmlFor="else">Anders</label>
-                <input name="delivery" ref={register({required: true})} id="else" type="radio" value="else"/>
-
-                <label htmlFor="comment">Opmerking
-                    <textarea name="textarea" ref={register({required: true})} id="comment"></textarea></label>
-
-                <button type="submit" ref={register}>Verzend</button>
-
-            </form>
+                </form>
+            </div>
         </>
     )
 
